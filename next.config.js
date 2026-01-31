ff8c67efb9a64dcdaf5bed942807955c09f9a6b2
Manikanta <orugantimanikanta24@gmail.com>
2026-01-31 11:34:46 +0530

    config: setup Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
